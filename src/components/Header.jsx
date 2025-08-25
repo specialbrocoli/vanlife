8c4f4f1c219918e552613a2b6652ac277f2d3ab3
specialbrocoli <undertone252passinb.undertone252@passinbox.com>
2025-08-25 16:28:14 +0200

    feat: ✨ Add responsive header component with mobile menu

---
+++ src/components/Header.jsx
@@ -0,0 +1,94 @@
+import { useState } from 'react';
+import { NavLink, Link } from 'react-router-dom';
+import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai';
+
+const Header = () => {
+  const navLinks = [
+    // { name: 'Host', path: '/host' },
+    { name: 'Vans', path: '/vans' },
+    { name: 'About', path: '/about' },
+  ];
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <>
+      <header className='flex justify-between items-center px-3 py-4 md:px-8'>
+        {/* logo */}
+        <Link
+          to='/'
+          className='uppercase font-bold text-2xl hover:underline underline-offset-4'
+        >
+          #VanLife
+        </Link>
+
+        {/* navbar menu */}
+        <nav>
+          <ul className='hidden md:flex font-normal gap-4'>
+            {navLinks.map((link) => (
+              <li key={link.name} className='p-2 text-lg'>
+                <NavLink
+                  to={link.path}
+                  className={({ isActive }) =>
+                    isActive ? 'underline underline-offset-4' : null
+                  }
+                >
+                  {link.name}
+                </NavLink>
+              </li>
+            ))}
+          </ul>
+        </nav>
+
+        {/* toggling menu on mobile */}
+        <button onClick={() => toggleMenu(!isOpen)} className='md:hidden z-10'>
+          {!isOpen ? (
+            <AiOutlineMenu size={28} color='#FF8C38' />
+          ) : (
+            <AiOutlineClose size={28} color='#FF8C38' />
+          )}
+        </button>
+      </header>
+
+      {/* mobile menu */}
+      <div
+        className={
+          isOpen
+            ? 'flex flex-col items-center justify-center min-h-screen bg-black/50 backdrop-blur-2xl absolute top-0 right-0 w-full sm:hidden'
+            : 'hidden'
+        }
+      >
+        {/* logo */}
+        <Link
+          to='/'
+          className='uppercase font-bold text-3xl hover:underline underline-offset-4 text-[#FF8C38]'
+        >
+          #VanLife
+        </Link>
+
+        {/* menu */}
+        <nav>
+          <ul className='flex flex-col items-center mt-6'>
+            {navLinks.map((link) => (
+              <li key={link.name} className='p-4'>
+                <NavLink
+                  to={link.path}
+                  onClick={() => setIsOpen(false)}
+                  className='text-white text-xl'
+                >
+                  {link.name}
+                </NavLink>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </div>
+    </>
+  );
+};
+
+export default Header;
