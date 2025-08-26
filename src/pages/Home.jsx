84d97eea1adc85283f236378934ad8989dda3eae
specialbrocoli <undertone252passinb.undertone252@passinbox.com>
2025-08-26 13:40:39 +0200

    feat: ✨ Add hero section to Home page with custom styles

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,24 @@
-import React from 'react';
+import CustomLink from '../components/Button';
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <section className='flex-1'>
+        <div className='bg-black/20 bg-[url(/home-hero.png)] bg-blend-overlay bg-no-repeat bg-cover bg-center rounded-md p-8 content-center text-left text-white space-y-4 h-4/5'>
+          <h1 className='text-3xl font-bold md:text-4xl'>
+            You got the travel plans, we got the travel vans.
+          </h1>
+
+          <p className='text-md md:text-lg'>
+            Add adventure to your life by joining the #vanlife movement. Rent
+            the perfect van to make your perfect road trip.
+          </p>
+
+          <CustomLink to='/vans'>Find your van</CustomLink>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Home;
