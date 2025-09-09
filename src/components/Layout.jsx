6c1e755fbb08549fb612566e32145fa44f369235
specialbrocoli <undertone252passinb.undertone252@passinbox.com>
2025-09-09 13:16:16 +0200

    fix: 🐛 Update import path for Outlet, resolve compatibility issue

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,6 +1,6 @@
 import Header from './Header';
 import Footer from './Footer';
-import { Outlet } from 'react-router-dom';
+import { Outlet } from 'react-router';
 
 const Layout = () => {
   return (
