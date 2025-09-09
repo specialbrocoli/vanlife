f1d93b5d889d7c2ee43ea59ea009195f8ec71350
specialbrocoli <undertone252passinb.undertone252@passinbox.com>
2025-09-09 13:17:06 +0200

    refactor: ♻️ Update van image URLs to use relative paths fix: 🐛 Fix vans API to return correct data

--- src/server.js
+++ src/server.js
@@ -13,8 +13,7 @@ createServer({
       price: 60,
       description:
         'The Modest Explorer is a van designed to get you out of the house and into nature. This beauty is equipped with solar panels, a composting toilet, a water tank and kitchenette. The idea is that you can pack up your home and escape for a weekend or even longer!',
-      imageUrl:
-        'https://assets.scrimba.com/advanced-react/react-router/modest-explorer.png',
+      imageUrl: '/modest-explorer.png',
       type: 'simple',
       hostId: '123',
     });
@@ -24,8 +23,7 @@ createServer({
       price: 80,
       description:
         "Beach Bum is a van inspired by surfers and travelers. It was created to be a portable home away from home, but with some cool features in it you won't find in an ordinary camper.",
-      imageUrl:
-        'https://assets.scrimba.com/advanced-react/react-router/beach-bum.png',
+      imageUrl: '/beach-bum.png',
       type: 'rugged',
       hostId: '123',
     });
@@ -35,8 +33,7 @@ createServer({
       price: 100,
       description:
         "Reliable Red is a van that was made for travelling. The inside is comfortable and cozy, with plenty of space to stretch out in. There's a small kitchen, so you can cook if you need to. You'll feel like home as soon as you step out of it.",
-      imageUrl:
-        'https://assets.scrimba.com/advanced-react/react-router/reliable-red.png',
+      imageUrl: '/reliable-red.png',
       type: 'luxury',
       hostId: '456',
     });
@@ -46,8 +43,7 @@ createServer({
       price: 65,
       description:
         'Dreamfinder is the perfect van to travel in and experience. With a ceiling height of 2.1m, you can stand up in this van and there is great head room. The floor is a beautiful glass-reinforced plastic (GRP) which is easy to clean and very hard wearing. A large rear window and large side windows make it really light inside and keep it well ventilated.',
-      imageUrl:
-        'https://assets.scrimba.com/advanced-react/react-router/dreamfinder.png',
+      imageUrl: '/dreamfinder.png',
       type: 'simple',
       hostId: '789',
     });
@@ -57,8 +53,7 @@ createServer({
       price: 120,
       description:
         'The Cruiser is a van for those who love to travel in comfort and luxury. With its many windows, spacious interior and ample storage space, the Cruiser offers a beautiful view wherever you go.',
-      imageUrl:
-        'https://assets.scrimba.com/advanced-react/react-router/the-cruiser.png',
+      imageUrl: '/the-cruiser.png',
       type: 'luxury',
       hostId: '789',
     });
@@ -68,8 +63,7 @@ createServer({
       price: 70,
       description:
         "With this van, you can take your travel life to the next level. The Green Wonder is a sustainable vehicle that's perfect for people who are looking for a stylish, eco-friendly mode of transport that can go anywhere.",
-      imageUrl:
-        'https://assets.scrimba.com/advanced-react/react-router/green-wonder.png',
+      imageUrl: '/green-wonder.png',
       type: 'rugged',
       hostId: '123',
     });
@@ -87,8 +81,8 @@ createServer({
     // this.timing = 1000;
 
     this.get('/vans', (schema, request) => {
-      // return schema.vans.all();
-      return new Response(400, {}, { error: 'Error fetching data' });
+      return schema.vans.all();
+      // return new Response(400, {}, { error: "Error fetching data" });
     });
 
     this.get('/vans/:id', (schema, request) => {
